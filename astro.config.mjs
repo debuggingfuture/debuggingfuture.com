87293d830262b07fc1b0be3a149748c64dc08139
debuggingfuture <lauchunyin@debuggingfuture.com>
2025-04-23 01:03:21 +0900

    fix: lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,28 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from "astro/config";
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
-
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://debuggingfuture.com',
+	site: "https://debuggingfuture.com",
 	integrations: [mdx(), sitemap()],
 	compressHTML: false,
 	vite: {
 		// @ts-ignore
-		plugins: [tailwindcss({
-			theme: {
-				extend: {
-					fontFamily: {
-						serif: ['Noto Serif TC', 'serif'], // Or 'sans' if you prefer
+		plugins: [
+			tailwindcss({
+				theme: {
+					extend: {
+						fontFamily: {
+							serif: ["Noto Serif TC", "serif"], // Or 'sans' if you prefer
+						},
 					},
 				},
-			},
-		})],
+			}),
+		],
 	},
 	image: {
 		service: passthroughImageService(),
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Debug 未來';
-export const SITE_DESCRIPTION = 'debuggingfuture';
+export const SITE_TITLE = "Debug 未來";
+export const SITE_DESCRIPTION = "debuggingfuture";
